f8d35c1ee29fc9f210e4a079ba2cf972d734f791
Thomas R. Nathan <43553816+ThomasRNathan@users.noreply.github.com>
2025-01-07 09:55:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Oction dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Oction dataset", page_icon="🔨")
+st.title("🏠 Movies dataset")
 st.write(
     """
     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
